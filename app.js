64f46706901cb5ae0cefaa678cfe1f465ee5eb95
Attritionist <55213784+Attritionist@users.noreply.github.com>
2024-07-26 22:28:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
+const fs = require("fs");
 
 const TELEGRAM_CHAT_ID = process.env["TELEGRAM_CHAT_ID"];
 const TELEGRAM_BOT_TOKEN = process.env["TELEGRAM_BOT_TOKEN"];
 const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"];
 const COINGECKO_API = process.env["COINGECKO_API"];
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
-const tokenDecimals = 18;
+const tokenDecimals = 8;  // Updated to 8 decimals as per the API response
 const initialSupply = 100000000;
-const fs = require("fs");
-const burnAnimation = "https://voidonbase.com/burn.jpg";
+const burnAnimation = "https://fluxonbase.com/burn.jpg";
+const buyAnimation = "https://fluxonbase.com/buy.jpg";  // New constant for buy image
+const YIN_CONTRACT_ADDRESS = '0xecb36ff12cbe4710e9be2411de46e6c180a4807f';
+const YANG_CONTRACT_ADDRESS = '0x384C9c33737121c4499C85D815eA57D1291875Ab';
 
 const processedTransactionsFilePath = "processed_transactions.json";
 let processedTransactions = new Set();
@@ -21,8 +24,7 @@ const POOL_MAPPING = {
   "0x90fbb03389061020eec7ce9a7435966363410b87": "YIN/ETH",
 };
 
-const REVERSED_POOLS = [
-];
+const REVERSED_POOLS = [];
 
 if (fs.existsSync(processedTransactionsFilePath)) {
   const data = fs.readFileSync(processedTransactionsFilePath, "utf-8");
@@ -52,9 +54,9 @@ function saveProcessedTransactions() {
 async function getYinPrice() {
   try {
     const response = await axios.get(
-      `https://pro-api.coingecko.com/api/v3/onchain/simple/networks/base/token_price/0xecb36ff12cbe4710e9be2411de46e6c180a4807f?x_cg_pro_api_key=${COINGECKO_API}`
+      `https://pro-api.coingecko.com/api/v3/onchain/simple/networks/base/token_price/${YIN_CONTRACT_ADDRESS}?x_cg_pro_api_key=${COINGECKO_API}`
     );
-    const tokenAddress = '0xecb36ff12cbe4710e9be2411de46e6c180a4807f'.toLowerCase();
+    const tokenAddress = YIN_CONTRACT_ADDRESS.toLowerCase();
     const yinPrice = response.data.data.attributes.token_prices[tokenAddress];
     return { yinPrice: parseFloat(yinPrice) };
   } catch (error) {
@@ -62,6 +64,7 @@ async function getYinPrice() {
     return null;
   }
 }
+
 
 setInterval(async () => {
   const priceInfo = await getYinPrice();
@@ -134,132 +137,6 @@ async function sendAnimationMessage(photo, options) {
   sendBurnFromQueue();
 }
 
-function getYangRank(yangBalance) {
-  const YANG_RANKS = {
-    "YANG Ultimate": 2000000,
-    "YANG Omega": 1500000,
-    "YANG Absolute": 1000000,
-    "YANG Singularity": 900000,
-    "YANG Omnipotence": 850000,
-    "YANG Eternity": 800000,
-    "YANG Apotheosis": 750000,
-    "YANG Cosmic Blazer": 696969,
-    "YANG Divine": 650000,
-    "YANG Celestial": 600000,
-    "YANG Exalted": 550000,
-    "YANG Transcendent": 500000,
-    "YANG Majesty": 450000,
-    "YANG Sovereign": 400000,
-    "YANG Monarch": 350000,
-    "YANG Admiral": 275000,
-    "YANG Warden": 250000,
-    "YANG Harbinger": 225000,
-    "YANG Evoker": 200000,
-    "YANG Emperor": 175000,
-    "YANG Assassin": 162500,
-    "YANG Overlord": 150000,
-    "YANG Creature": 140000,
-    "YANG Hierophant": 130000,
-    "YANG Juggernaut": 120000,
-    "YANG Grandmaster": 110000,
-    "YANG Lord": 100000,
-    "YANG Alchemist": 92500,
-    "YANG Clairvoyant": 85000,
-    "YANG Conjurer": 80000,
-    "YANG Archdruid": 75000,
-    "YANG Dank Mystic": 69420,
-    "YANG Archmage": 65000,
-    "YANG Warlock": 60000,
-    "YANG Sorcerer": 55000,
-    "YANG Knight": 50000,
-    "YANG Shaman": 45000,
-    "YANG Sage": 40000,
-    "YANG Warrior": 35000,
-    "YANG Enchanter": 30000,
-    "YANG Seer": 27500,
-    "YANG Necromancer": 25000,
-    "YANG Summoner": 22500,
-    "YANG Master": 20000,
-    "YANG Disciple": 15000,
-    "YANG Acolyte": 12500,
-    "YANG Expert": 10000,
-    "YANG Apprentice": 7500,
-    "YANG Rookie": 5000,
-    "YANG Learner": 2500,
-    "YANG Initiate": 1000,
-    "YANG Peasant": 1
-  };
-
-  let yangRank = "YANG Peasant";
-  for (const [rank, threshold] of Object.entries(YANG_RANKS)) {
-    if (yangBalance >= threshold) {
-      yangRank = rank;
-      break;
-    }
-  }
-
-  return yangRank;
-}
-
-function getRankImageUrl(yangRank) {
-  const rankToImageUrlMap = {
-    "YANG Peasant": "https://voidonbase.com/rank1.png",
-    "YANG Initiate": "https://voidonbase.com/rank2.png",
-    "YANG Learner": "https://voidonbase.com/rank3.png",
-    "YANG Rookie": "https://voidonbase.com/rank4.png",
-    "YANG Apprentice": "https://voidonbase.com/rank5.png",
-    "YANG Expert": "https://voidonbase.com/rank6.png",
-    "YANG Acolyte": "https://voidonbase.com/rank10.png",
-    "YANG Disciple": "https://voidonbase.com/rank11.png",
-    "YANG Master": "https://voidonbase.com/rank12.png",
-    "YANG Summoner": "https://voidonbase.com/rank14.png",
-    "YANG Necromancer": "https://voidonbase.com/rank15.png",
-    "YANG Seer": "https://voidonbase.com/rank16.png",
-    "YANG Enchanter": "https://voidonbase.com/rank17.png",
-    "YANG Warrior": "https://voidonbase.com/rankwar.png",
-    "YANG Sage": "https://voidonbase.com/rank18.png",
-    "YANG Shaman": "https://voidonbase.com/rank19.png",
-    "YANG Knight": "https://voidonbase.com/rank20.png",
-    "YANG Sorcerer": "https://voidonbase.com/rank21.png",
-    "YANG Warlock": "https://voidonbase.com/rank22.png",
-    "YANG Archmage": "https://voidonbase.com/rank24.png",
-    "YANG Dank Mystic": "https://voidonbase.com/420.png",
-    "YANG Archdruid": "https://voidonbase.com/rank25.png",
-    "YANG Conjurer": "https://voidonbase.com/rank26.png",
-    "YANG Clairvoyant": "https://voidonbase.com/rank27.png",
-    "YANG Alchemist": "https://voidonbase.com/rank28.png",
-    "YANG Lord": "https://voidonbase.com/rank29.png",
-    "YANG Grandmaster": "https://voidonbase.com/rankgm.png",
-    "YANG Juggernaut": "https://voidonbase.com/rankjug.png",
-    "YANG Hierophant": "https://voidonbase.com/rank30.png",
-    "YANG Creature": "https://voidonbase.com/rank32.png",
-    "YANG Overlord": "https://voidonbase.com/rank33.png",
-    "YANG Assassin": "https://voidonbase.com/assassin.png",
-    "YANG Emperor": "https://voidonbase.com/rank34.png",
-    "YANG Evoker": "https://voidonbase.com/rank35.png",
-    "YANG Harbinger": "https://voidonbase.com/rank36.png",
-    "YANG Warden": "https://voidonbase.com/rank39.png",
-    "YANG Admiral": "https://voidonbase.com/rank40.png",
-    "YANG Monarch": "https://voidonbase.com/rank41.png",
-    "YANG Sovereign": "https://voidonbase.com/rank42.png",
-    "YANG Majesty": "https://voidonbase.com/rank43.png",
-    "YANG Transcendent": "https://voidonbase.com/rank44.png",
-    "YANG Exalted": "https://voidonbase.com/rank45.png",
-    "YANG Celestial": "https://voidonbase.com/rank46.png",
-    "YANG Divine": "https://voidonbase.com/rank47.png",
-    "YANG Cosmic Blazer": "https://voidonbase.com/696969.png",
-    "YANG Apotheosis": "https://voidonbase.com/rank48.png",
-    "YANG Eternity": "https://voidonbase.com/rank49.png",
-    "YANG Omnipotence": "https://voidonbase.com/rank50.png",
-    "YANG Singularity": "https://voidonbase.com/rank51.png",
-    "YANG Absolute": "https://voidonbase.com/rank52.png",
-    "YANG Omega": "https://voidonbase.com/rank53.png",
-    "YANG Ultimate": "https://voidonbase.com/rank54.png"
-  };
-
-  return rankToImageUrlMap[yangRank] || "https://voidonbase.com/rank1.png";
-}
-
 async function detectUniswapLatestTransaction() {
   const poolAddresses = Object.keys(POOL_MAPPING);
 
@@ -305,7 +182,7 @@ async function detectUniswapLatestTransaction() {
           const transactionvalue = transaction.attributes.volume_in_usd;
           const marketCap = yinPrice * totalSupply;
 
-          const balanceDetailsUrl = `https://api.basescan.org/api?module=account&action=tokenbalance&contractaddress=0xecb36ff12cbe4710e9be2411de46e6c180a4807f&address=${fromAddress}&tag=latest&apikey=${ETHERSCAN_API_KEY}`;
+        const balanceDetailsUrl = `https://api.basescan.org/api?module=account&action=tokenbalance&contractaddress=${YIN_CONTRACT_ADDRESS}&address=${fromAddress}&tag=latest&apikey=${ETHERSCAN_API_KEY}`;
 
           const config = {
             headers: {
@@ -320,7 +197,6 @@ async function detectUniswapLatestTransaction() {
             const yinBalance = balanceDetailResponse.data.result / 10 ** tokenDecimals;
 
             if (isBuy && Number(transaction.attributes.volume_in_usd) > 50) {
-              // Handle normal buy transaction
               const emojiCount = Math.min(Math.ceil(transaction.attributes.volume_in_usd / 50), 96);
               let emojiString = "";
 
@@ -328,26 +204,20 @@ async function detectUniswapLatestTransaction() {
                 emojiString += "☯️🔥";
               }
 
-              const yangRank = getYangRank(yinBalance);
-              const imageUrl = getRankImageUrl(yangRank);
-
               const message = `${emojiString}
 💸 Bought ${amountTransferred.toFixed(2)} YIN ($${transactionvalue}) (<a href="${addressLink}">View Address</a>)
 ☯️ YIN Price: $${yinPrice.toFixed(5)}
 💰 Market Cap: $${marketCap.toFixed(0)}
 🔥 Percent Burned: ${percentBurned.toFixed(3)}%
 <a href="${chartLink}">📈 Chart</a>
-<a href="${txHashLink}">💱 TX Hash</a>
-⚖️ Remaining YIN Balance: ${yinBalance.toFixed(2)}
-🛡️ YANG Rank: ${yangRank}
-🚰 Pool: ${POOL_MAPPING[poolAddress]}`;
+<a href="${txHashLink}">💱 TX Hash</a>`;
 
               const yinMessageOptions = {
                 caption: message,
                 parse_mode: "HTML",
               };
 
-              sendPhotoMessage(imageUrl, yinMessageOptions);
+              sendPhotoMessage(buyAnimation, yinMessageOptions);  // Use buyAnimation for buy transactions
               processedUniswapTransactions.add(transaction.id);
             } else {
               processedUniswapTransactions.add(transaction.id);
@@ -361,63 +231,54 @@ async function detectUniswapLatestTransaction() {
     }
   });
 }
-async function detectYangBurnEvent() {
+
+let currentTotalSupply = BigInt(0);
+
+async function checkTotalSupply() {
   try {
-    const config = {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows; Windows NT 10.4; WOW64; en-US) AppleWebKit/537.20 (KHTML, like Gecko) Chrome/53.0.3086.259 Safari/602.4 Edge/12.29796'
-      },
-      withCredentials: true
-    };
+const apiUrl = `https://api.basescan.org/api?module=stats&action=tokensupply&contractaddress=${YANG_CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}`;
+    const response = await axios.get(apiUrl);
 
-    const apiUrl = `https://api.basescan.org/api?module=account&action=tokentx&contractaddress=0x384C9c33737121c4499C85D815eA57D1291875Ab&address=0x0000000000000000000000000000000000000000&page=1&offset=1&sort=desc&apikey=${ETHERSCAN_API_KEY}`;
-    const response = await axios.get(apiUrl, config);
-
-    if (response.data.status !== "1") {
-      throw new Error("Failed to retrieve token transactions");
+    if (response.data.status === "1") {
+      const newTotalSupply = BigInt(response.data.result);
+      
+      if (currentTotalSupply === BigInt(0)) {
+        // First run, just set the initial supply
+        currentTotalSupply = newTotalSupply;
+        console.log(`Initial total supply set to: ${currentTotalSupply}`);
+      } else if (newTotalSupply < currentTotalSupply) {
+        // A burn has occurred
+        const burnedAmount = currentTotalSupply - newTotalSupply;
+        const burnedAmountFormatted = Number(burnedAmount) / 10**tokenDecimals;
+        
+        await reportBurn(burnedAmountFormatted);
+        
+        currentTotalSupply = newTotalSupply;
+      }
+    } else {
+      throw new Error("Failed to retrieve total supply");
     }
-
-    await updateTotalBurnedAmount();
-
-    const newBurnEvents = response.data.result.filter(
-      (transaction) =>
-        transaction.to.toLowerCase() ===
-        "0x0000000000000000000000000000000000000000" &&
-        !processedTransactions.has(transaction.hash)
-    );
-
-    if (newBurnEvents.length === 0) {
-      console.log("No new burn events detected.");
-      return;
-    }
-
-    newBurnEvents.forEach((transaction) => {
-      processedTransactions.add(transaction.hash);
-      const amountBurned =
-        Number(transaction.value) / 10 ** tokenDecimals;
-      const txHash = transaction.hash;
-      const txHashLink = `https://basescan.org/tx/${txHash}`;
-      const chartLink = "https://dexscreener.com/base/0x384C9c33737121c4499C85D815eA57D1291875Ab";
-      const percentBurned =
-        ((initialSupply - totalBurnedAmountt) / initialSupply) * 100;
-      totalBurnedAmountt += amountBurned;
-      const burnMessage = `YANG Burned!\n\n💀💀💀💀💀\n🔥 Burned: ${amountBurned.toFixed(
-        3
-      )} YANG\nPercent Burned: ${percentBurned.toFixed(
-        2
-      )}%\n🔎 <a href="${chartLink}">Chart</a> | <a href="${txHashLink}">TX Hash</a>`;
-
-      const burnanimationMessageOptions = {
-        caption: burnMessage,
-        parse_mode: "HTML",
-      };
-      sendAnimationMessage(burnAnimation, burnanimationMessageOptions);
-
-      saveProcessedTransactions();
-    });
   } catch (error) {
-    console.error("Error detecting token burn event:", error);
+    console.error("Error checking total supply:", error);
   }
+}
+
+async function reportBurn(burnedAmount) {
+  const chartLink = "https://dexscreener.com/base/0x384C9c33737121c4499C85D815eA57D1291875Ab";
+  const percentBurned = ((initialSupply - Number(currentTotalSupply) / 10**tokenDecimals) / initialSupply) * 100;
+  
+  const burnMessage = `YANG Burned!\n\n💀💀💀💀💀\n🔥 Burned: ${burnedAmount.toFixed(3)} YANG\nPercent Burned: ${percentBurned.toFixed(2)}%\n🔎 <a href="${chartLink}">Chart</a>`;
+
+  const burnAnimationMessageOptions = {
+    caption: burnMessage,
+    parse_mode: "HTML",
+  };
+  
+  sendAnimationMessage(burnAnimation, burnAnimationMessageOptions);
+}
+
+async function detectYangBurnEvent() {
+  await checkTotalSupply();
 }
 
 function scheduleNextCall(callback, delay) {
@@ -429,7 +290,6 @@ function scheduleNextCall(callback, delay) {
 }
 
 let totalBurnedAmount = 0;
-let totalBurnedAmountt = 0;
 
 async function updateTotalBurnedAmount() {
   try {
@@ -440,20 +300,18 @@ async function updateTotalBurnedAmount() {
       withCredentials: true
     };
 
-    const apiUrl = `https://api.basescan.org/api?module=account&action=tokenbalance&contractaddress=0x384C9c33737121c4499C85D815eA57D1291875Ab&address=0x0000000000000000000000000000000000000000&apikey=${ETHERSCAN_API_KEY}`;
+const apiUrl = `https://api.basescan.org/api?module=stats&action=tokensupply&contractaddress=${YANG_CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}`;
     const response = await axios.get(apiUrl, config);
 
     if (response.data.status === "1") {
-      const balance = Number(response.data.result) / 10 ** tokenDecimals;
-      totalBurnedAmount = balance;
-      totalBurnedAmountt = initialSupply - balance;
+      const currentSupply = BigInt(response.data.result);
+      totalBurnedAmount = Number(BigInt(initialSupply * 10**tokenDecimals) - currentSupply) / 10**tokenDecimals;
+      console.log(`Total burned amount updated: ${totalBurnedAmount}`);
     }
   } catch (error) {
     console.error("Error updating total burned amount:", error);
   }
 }
-
-scheduleNextCall(detectYangBurnEvent, 30000);
 
 // Add initial 300 transactions to processed transactions set to avoid spamming the group on initial startup
 const fetchInitialUniswapTransactions = async () => {
@@ -478,8 +336,17 @@ const fetchInitialUniswapTransactions = async () => {
   }
 }
 
-fetchInitialUniswapTransactions().catch((error) => {
-  console.error("Error fetching initial Uniswap transactions:", error);
-}).then(() => {
-  scheduleNextCall(detectUniswapLatestTransaction, 10000);
-});
+// Initialize and start the bot
+updateTotalBurnedAmount()
+  .then(() => {
+    console.log("Total burned amount initialized.");
+    scheduleNextCall(detectYangBurnEvent, 20000); // Check for burns every 20 seconds
+    return fetchInitialUniswapTransactions();
+  })
+  .then(() => {
+    console.log("Initial Uniswap transactions fetched.");
+    scheduleNextCall(detectUniswapLatestTransaction, 5000); // Check for new Uniswap transactions every 5 seconds
+  })
+  .catch((error) => {
+    console.error("Error during initialization:", error);
+  });
