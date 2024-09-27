2e656158a3ff51a1ae0d2b085224d77768035776
The Void / FLUX <55213784+Attritionist@users.noreply.github.com>
2024-09-27 04:47:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -812,7 +812,7 @@ async function reportYangBurn(burnedAmount, previousTotalSupply) {
   const fluxData = await getFluxData();
   const currentPrice = fluxData ? fluxData.yangPrice : await getCurrentYangPrice();
 
-  const burnMessage = `YANG Burned!\n\n☀️☀️☀️☀️☀️\n🔥 Burned: ${burnedAmount.toFixed(4)} YANG (${newlyBurnedPercent.toFixed(6)}%)\n🔥 Total Burned: ${yangTotalBurnedAmount.toFixed(4)} YANG\n🔥 Percent Burned: ${percentBurned.toFixed(3)}%\n☯️ YANG to YIN ratio: ${currentPrice.toFixed(4}`;
+  const burnMessage = `YANG Burned!\n\n☀️☀️☀️☀️☀️\n🔥 Burned: ${burnedAmount.toFixed(4)} YANG (${newlyBurnedPercent.toFixed(6)}%)\n🔥 Total Burned: ${yangTotalBurnedAmount.toFixed(4)} YANG\n🔥 Percent Burned: ${percentBurned.toFixed(3)}%\n☯️ YANG to YIN ratio: ${currentPrice.toFixed(4)}`;
 
   const burnAnimationMessageOptions = {
     caption: burnMessage,
